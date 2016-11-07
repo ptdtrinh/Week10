5d0a14a4f746a855c1ff4ad7efc5ca3fd77422aa
6119-16\6119 <ptdoantrinh2691@gmail.com>
2016-11-08 04:23:10 +0900

    fafsfsfsa

--- Week10/Week10/Source.cpp
+++ Week10/Week10/Source.cpp
@@ -16,8 +16,20 @@ public:
 
 int main()
 {
-	const float study_time_data[3] = { 0, 1, 2 };
-	const float score_data[3] = { 0, 2, 4 };
+	int num_data = 3;
+	const float study_time_data[num_data] = { 0, 1, 2 };
+	const float score_data[num_data] = { 0, 2, 4 };
 
+	LinearHypothesis lh;
+
+	for (int i = 0; i < num_data; i++)
+	{
+		const float y_output = lh.getY(study_time_data[i]);
+		const float y_target = score_data[i];
+		const float error = y_output - y_target;
+
+		const float sqr_error = error * error; //always 0 or positive
+		std::cout <<  << std::endl;
+	}
 	return 0;
 }
